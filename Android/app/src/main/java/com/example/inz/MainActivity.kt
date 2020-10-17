f9ed377c30ef8b39d0ba44be1f2ecc0dce52026e
Arkadiusz Kruszyński <arkadiusz.h.kruszynski@gmail.com>
2020-10-17 18:45:23 +0200

    Add basic SplashScreen

--- Android/app/src/main/java/com/example/inz/MainActivity.kt
+++ Android/app/src/main/java/com/example/inz/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.inz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
     }
 }

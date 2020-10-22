84300851800088dab5ffe05e73de17bb97a90c81
Arkadiusz Kruszyński <arkadiusz.h.kruszynski@gmail.com>
2020-10-22 21:48:46 +0200

    .

--- Android/app/src/main/java/com/example/inz/MainActivity.kt
+++ Android/app/src/main/java/com/example/inz/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.inz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        val btnOpenMainView: Button = findViewById(R.id.button_login)
+        btnOpenMainView.setOnClickListener{
+            val intent = Intent(this, MainView::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }

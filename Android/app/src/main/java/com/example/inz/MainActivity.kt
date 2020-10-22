77245cda43f94a08f824ffd4d7b3a2ea1b68b2a1
keti98 <aniakettner@gmail,com>
2020-10-23 00:20:05 +0200

    fix bugs

--- Android/app/src/main/java/com/example/inz/MainActivity.kt
+++ Android/app/src/main/java/com/example/inz/MainActivity.kt
@@ -13,18 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
-
-<<<<<<< HEAD
-        // editTextTextLogin.setColor(Color.WHITE);
-        //editTextTextPassword.setColor(Color.WHITE);
-=======
-        val btnOpenMainView: Button = findViewById(R.id.button_login)
-        btnOpenMainView.setOnClickListener{
-            val intent = Intent(this, MainView::class.java)
-            startActivity(intent)
-        }
-
->>>>>>> 66ba46f6347237a32ce941ec6cc9b16b7efa61ce
+        
 
     }
 }

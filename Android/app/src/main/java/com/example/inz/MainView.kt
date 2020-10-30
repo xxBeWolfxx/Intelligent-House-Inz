0ff86030fc59439087af44a77faef769b92221b0
Arkadiusz Kruszyński <arkadiusz.h.kruszynski@gmail.com>
2020-10-30 23:05:40 +0100

    Add nav_viewer

--- Android/app/src/main/java/com/example/inz/MainView.kt
+++ Android/app/src/main/java/com/example/inz/MainView.kt
@@ -2,10 +2,20 @@ package com.example.inz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.widget.Toast
 
 class MainView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainview)
+        val nav_view = findViewById<NavigationView>(R.id.nav_view)
+        nav_view.setNavigationItemSelectedListener { menuItem ->
+            if(menuItem.itemId == R.id.nav_btn1){
+                Toast.makeText(applicationContext, "Przycisk 1", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+
     }
 }
